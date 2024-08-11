6e9de1e43157234e4c36dbb233843ac78d601e71
Zeenat-nazar-muhammad <angbird74@gmail.com>
2024-08-11 12:15:06 +0500

    Add files via upload

---
+++ assingment 5/assingment.py/assingment.5.py
@@ -0,0 +1,11 @@
+
+def max(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+print(max(67,89,56))
+print(max(30,78,21))
+print(max(15,10,20))
